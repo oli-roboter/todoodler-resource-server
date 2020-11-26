989d707eadb2516462d59d24eb8511ba477cdc44
Oliver Wagner <oliverthomas.wagner@gmail.com>
2020-11-26 11:08:35 +0000

    Cahnged env variable name for clarity

--- server/config/config.js
+++ server/config/config.js
@@ -4,13 +4,13 @@ dotenv.config();
 
 const {
   NODE_ENV,
-  SERVER_PORT,
+  TODOODLER_PORT,
   MONGO_ATLAS_URL,
   AUTH_BACKEND,
 } = process.env;
 
 const MONGO_DB = MONGO_ATLAS_URL;
-const PORT = SERVER_PORT;
+const PORT = TODOODLER_PORT;
 
 export {
   NODE_ENV,
